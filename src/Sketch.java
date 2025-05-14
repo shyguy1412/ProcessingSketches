d18ad63cd90cda7c2537c0a78fcfcb90699fd6e8
Shy <48560522+shyguy1412@users.noreply.github.com>
2025-05-14 15:07:29 +0000

    Initial commit

---
+++ src/Sketch.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("Sketch");
+    }
+
+    @Override
+    public void settings() {
+        size(500, 500);
+    }
+
+    @Override
+    public void setup() {
+    }
+    
+    @Override
+    public void draw() {
+        background(0);
+    }
+
+}
