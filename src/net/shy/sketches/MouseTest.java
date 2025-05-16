8f61f953e9c5b51d446e23f405173c9dbf51eb83
Shy <shyguyx1412@gmail.com>
2025-05-16 18:11:56 +0000

    update mouse test for new simplegui version

--- src/net/shy/sketches/MouseTest.java
+++ src/net/shy/sketches/MouseTest.java
@@ -6,6 +6,7 @@ import net.shy.simplegui.events.SClickListener;
 import net.shy.simplegui.events.SClickable;
 
 public class MouseTest extends Sketch implements SClickListener {
+  SButton btn;
 
   @Override
   public void settings() {
@@ -14,13 +15,14 @@ public class MouseTest extends Sketch implements SClickListener {
 
   @Override
   public void setup() {
-    SButton btn = new SButton(this, width / 2 - 50, height / 2 - 25, 100, 50);
+    btn = new SButton(this, width / 2 - 50, height / 2 - 25, 100, 50);
     btn.setLabel("CLICK ME");
     btn.addClickListener(this);
   }
 
   @Override
   public void draw() {
+    btn.draw();
   }
 
   @Override
